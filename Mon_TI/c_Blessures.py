ff8d6288135207f286ec023f7fb4081e04a04a80
Erwan <erwan.gretillat@soprasteria.com>
2024-02-20 16:11:13 +0100

    Gestion des blessures dans un DF

--- Mon_TI/c_Blessures.py
+++ Mon_TI/c_Blessures.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,19 +17,24 @@ if response.status_code == 200:
     soup = BeautifulSoup(html_content, "html.parser") # Analyser le HTML avec BeautifulSoup
 
     # Trouver tous les éléments de la page avec les classes "col-name Table__TD" et "col-stat Table__TD"
-    all_players = soup.find_all("td", class_=["col-name Table__TD", "col-stat Table__TD"])
+    all_players = soup.find_all("td", class_=["col-name Table__TD","col-date Table__TD", "col-stat Table__TD","col-desc Table__TD"])
     
-    # col-name Table__TD / nom
-    # col-pos Table__TD / poste
-    # col-date Table__TD / date de retour
-    # col-stat Table__TD / statut
-    # col-desc Table__TD / commentaire
-
-    # Afficher les informations sur les blessures
-    for i in range(0, len(all_players), 2):  # Parcourir la liste par pas de 2 pour obtenir le nom et le statut
+    # Initialiser une liste pour stocker les données des joueurs
+    player_data = []
+    
+    # Parcourir les informations des joueurs et les stocker dans la liste player_data
+    for i in range(0, len(all_players), 4):
         player_name = all_players[i].text.strip()
-        player_status = all_players[i+1].text.strip()
-        print(f"Nom du joueur : {player_name}, Statut : {player_status}")
+        player_returnDate = all_players[i+1].text.strip()
+        player_status = all_players[i+2].text.strip()
+        player_comment = all_players[i+3].text.strip()
+        player_data.append([player_name, player_returnDate, player_status, player_comment])
+
+    # Créer un DataFrame pandas à partir des données des joueurs
+    df = pd.DataFrame(player_data, columns=['Joueur', 'Date de retour', 'Statut', 'Commentaire'])
+
+    # Afficher le DataFrame
+    print(df)
 
 else:
     print("La requête a échoué avec le code de statut:", response.status_code)
