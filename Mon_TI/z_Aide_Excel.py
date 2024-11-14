53b853c0eca9373aba20588fab8bbaabbf60f925
egretillat <erwan.gretillat@soprasteria.com>
2024-11-14 11:14:38 +0100

    Aide Excel

--- Mon_TI/z_Aide_Excel.py
+++ Mon_TI/z_Aide_Excel.py
@@ -1,11 +1,19 @@
 from x_Utilitaires import *
 
-print(obtenir_joueurID_avec_joueurNom("John Collins"))
+def obtenirIDjoueur():
+    print(obtenir_joueurID_avec_joueurNom("John Collins"))
 
-ids_joueurs = [1630578, 1641708]
+def obtenirListeIDsEquipes():
+    ids_joueurs = [1630578, 1641708]
 
-for player_id in ids_joueurs:
-    DF_joueur_info = commonplayerinfo.CommonPlayerInfo(player_id).get_data_frames()[0]
-    TEAM_CITY = DF_joueur_info['TEAM_CITY'].iloc[0]
-    TEAM_NAME = DF_joueur_info['TEAM_NAME'].iloc[0]
-    print(TEAM_CITY + " " + TEAM_NAME)
+    for player_id in ids_joueurs:
+        DF_joueur_info = commonplayerinfo.CommonPlayerInfo(player_id).get_data_frames()[0]
+        TEAM_CITY = DF_joueur_info['TEAM_CITY'].iloc[0]
+        TEAM_NAME = DF_joueur_info['TEAM_NAME'].iloc[0]
+        print(TEAM_CITY + " " + TEAM_NAME)
+
+# ------------------------------
+# Point d'entrée du programme
+if __name__ == "__main__":
+    obtenirIDjoueur()
+    #obtenirListeIDsEquipes()
